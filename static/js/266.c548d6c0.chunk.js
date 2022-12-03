c4848cb770c3eb5b46e625c6ef21faba83fe7edb
GoDmitrAn <99049391+GoDmitrAn@users.noreply.github.com>
2022-12-03 23:08:03 +0000

    Deploying to gh-pages from @ GoDmitrAn/goit-react-hw-08-phonebook@4d051032fe5d6e45165344fbd45d19b76a12b55a 🚀

---
+++ static/js/266.c548d6c0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{266:function(e,n,a){a.r(n);var i=a(184);n.default=function(){return(0,i.jsx)("main",{children:(0,i.jsx)("div",{children:(0,i.jsxs)("h1",{children:[" ","Contact manager welcome page"," ",(0,i.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})})}}}]);
+//# sourceMappingURL=266.c548d6c0.chunk.js.map
