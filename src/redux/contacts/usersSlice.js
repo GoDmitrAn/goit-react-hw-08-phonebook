8f06da83c5e432409503c4153d5dc786464a156e
Dmytro Holombytsia <iqbitsak@gmail.com>
2022-12-04 02:07:09 +0200

    builder in contactsSlice

--- src/redux/contacts/usersSlice.js
+++ src/redux/contacts/usersSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { logout } from 'redux/auth/operations';
 import { addUser, deleteUser, fetchContacts } from './operations';
 
@@ -17,39 +17,48 @@ const usersSlice = createSlice({
     error: null,
   },
 
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [fetchContacts.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-    [fetchContacts.rejected]: handleRejected,
-
-    [addUser.pending]: handlePending,
-    [addUser.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items.push(action.payload);
-    },
-    [addUser.rejected]: handleRejected,
-
-    [deleteUser.pending]: handlePending,
-    [deleteUser.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const index = state.items.findIndex(
-        task => task.id === action.payload.id
-      );
-      state.items.splice(index, 1);
-    },
-    [deleteUser.rejected]: handleRejected,
-  },
-  [logout.fulfilled](state) {
-    state.items = [];
-    state.error = null;
-    state.isLoading = false;
-  },
+  extraReducers: builder =>
+    builder
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.items = action.payload;
+      })
+      .addCase(addUser.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        const index = state.items.findIndex(
+          task => task.id === action.payload.id
+        );
+        state.items.splice(index, 1);
+      })
+      .addCase(logout.fulfilled, state => {
+        state.items = [];
+      })
+      .addMatcher(
+        isAnyOf(fetchContacts.pending, addUser.pending, deleteUser.pending),
+        state => {
+          state.isLoading = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(fetchContacts.rejected, addUser.rejected, deleteUser.rejected),
+        (state, action) => {
+          state.isLoading = false;
+          state.error = action.payload;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchContacts.fulfilled,
+          addUser.fulfilled,
+          deleteUser.fulfilled,
+          logout.fulfilled
+        ),
+        state => {
+          state.isLoading = false;
+          state.error = null;
+        }
+      ),
 });
 
 export const usersReducer = usersSlice.reducer;
